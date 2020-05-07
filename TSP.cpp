5b6eb57db9f881cd033e939743aae3e48ce7d9fe
SamYassien <syassien@smu.edu>
2020-05-07 08:31:59 -0500

    two solutions

--- TSP.cpp
+++ TSP.cpp
@@ -1,5 +1,87 @@
 #include "TSP.h"
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <climits>
 
-class TSP{
+TSP::~TSP() {
+    delete graph;
+}
 
-};
+void TSP::readGraph( const std::string& FileName) {
+    std::ifstream in(FileName);
+    if (!in) {
+        std::cout << "Couldn't read file. Exiting... " << std::endl;
+    }
+    int temp;
+    in >> temp ;
+    size = temp;
+    graph = new Graph(size);
+    for ( int i = 0 ; i < size*size ; i ++ ){
+            in >> temp;
+            graph->matrix[i] = temp ;
+    }
+    in.close();
+}
+
+int TSP::bruteForce(int s, std::vector<int>& minPath) {
+        int source = s;
+        std::vector <int> vertices;
+        for (int i = 0; i < size; i++)
+            if (i != source)
+                vertices.push_back(i);
+
+        int path = INT_MAX;
+        std::vector<int> tempPath;
+        do {
+            int cost = 0;
+            int current = source;
+            for (int i = 0 ; i < vertices.size() ; i++) {
+                tempPath.push_back(current);
+                cost += graph->matrix[graph->index( current, vertices[i] )];
+                current = vertices[i];
+            }
+            tempPath.push_back(current);
+            cost += graph->matrix[graph->index(current, source)];
+            if ( cost < path ){
+                path = cost;
+                minPath = tempPath;
+            }
+
+            tempPath.clear();
+        } while (std::next_permutation(vertices.begin(), vertices.end()));
+
+        minPath.push_back(source);
+        return path;
+}
+
+int TSP::DP(int s) {
+    std::vector<std::vector<int>> dp(size);
+    for(auto& i : dp)
+        i = std::vector<int>((1 << size) - 1, INT_MAX);
+
+    return DP(s, 1, dp);
+}
+
+int TSP::DP(int ind, int visited, std::vector<std::vector<int>> & dp) {
+    if(visited == ((1 << size) - 1))
+        return graph->matrix[graph->index( ind, 0)];
+
+    if(dp[ind][visited] != INT_MAX)
+        return dp[ind][visited];
+
+    for(int i = 0; i < size ; i++) {
+        if (!(visited & (1 << i))) {
+
+            int dist = graph->matrix[graph->index(ind, i)] + DP(i, visited | (1 << i), dp);
+
+            dp[ind][visited] = std::min(dp[ind][visited], dist);
+        }
+    }
+    return dp[ind][visited];
+}
+
+void TSP::print() {
+    graph->print();
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include "TSP.h"
+#include <chrono>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    TSP test;
+    std::vector<int> result;
+    test.readGraph("input.txt");
+
+    ofstream out("output.txt");
+///////////////// Running Bruteforce solution ///////////////////
+
+    out << "Brute Force Solution:\n" << endl;
+
+    auto start = std::chrono::system_clock::now();
+
+    out << "Minimum Cost: " << test.bruteForce(0, result) << endl;
+
+    auto end = std::chrono::system_clock::now();
+
+    out << "Brute force runtime: " << std::chrono::duration_cast<std::chrono::microseconds >(end - start).count() << " milliseconds.\n" << endl;
+
+    // Uncomment to see the path
+/*
+    for ( auto& i : result )
+        out << i << " ";
+    out << endl;
+*/
+
+///////////////// Running DP solution ///////////////////
+
+    out << "Dynamic Programming Solution:\n" << endl;
+    start = std::chrono::system_clock::now();
+
+    out << "Minimum Cost: " << test.DP(0) << endl;
+
+    end = std::chrono::system_clock::now();
+
+    out << "DP runtime: " << std::chrono::duration_cast<std::chrono::microseconds >(end - start).count() << " milliseconds.\n" << endl;
+
     return 0;
 }
